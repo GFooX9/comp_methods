aa1741021437a2bb7eded61dcd7d51e201730d0b
Dobler Alexander <147134471+GFooX9@users.noreply.github.com>
2025-05-12 11:53:15 +0500

    Add files via upload

---
+++ work_2.py
@@ -0,0 +1,56 @@
+from openpyxl import load_workbook
+
+
+def check_correct_matrix(matrix: list) -> bool:
+    for i in range(len(matrix)):
+        if abs(matrix[i][i]) * 2 < sum(list(map(lambda x: abs(x), matrix[i][:-1]))):
+            return False
+    return True
+
+
+def solve(matrix: list, pre_x: list):
+    global counter, accuracy
+    next_x1 = (pre_x[1] * matrix[0][1] + pre_x[2] * matrix[0][2] + pre_x[3] * matrix[0][3] + matrix[0][-1]) / matrix[0][0]
+    next_x2 = (pre_x[0] * matrix[1][0] + pre_x[2] * matrix[1][2] + pre_x[3] * matrix[1][3] + matrix[1][-1]) / matrix[1][1]
+    next_x3 = (pre_x[0] * matrix[2][0] + pre_x[1] * matrix[2][1] + pre_x[3] * matrix[2][3] + matrix[2][-1]) / matrix[2][2]
+    next_x4 = (pre_x[0] * matrix[3][0] + pre_x[1] * matrix[3][1] + pre_x[2] * matrix[3][2] + matrix[3][-1]) / matrix[3][3]
+    d1 = abs(pre_x[0] - next_x1)
+    d2 = abs(pre_x[1] - next_x2)
+    d3 = abs(pre_x[2] - next_x3)
+    d4 = abs(pre_x[3] - next_x4)
+    table_list.append([counter, pre_x[0], pre_x[1], pre_x[2], pre_x[3], d1, d2, d3, d4])
+    counter += 1
+    if max(d1, d2, d3, d4) <= accuracy:
+        return [matrix]
+    else:
+        return solve(matrix, pre_x=[next_x1, next_x2, next_x3, next_x4])
+
+
+if __name__ == "__main__":
+    accuracy = float(input("Введите точность вычислений:"))
+    file_name_table = "table_of_solve.xlsx"
+    table = load_workbook(filename=file_name_table)
+    table_list = table['Data']
+    table_list.append(['Counter', 'x1', "x2", 'x3', "x4", "d1", "d2", "d3", "d4"])
+    counter = 0
+    x_lst = [0 for i in range(4)]
+    """
+    start_matrix = [[28.0, 9.0, -3.0, -7.0, -159.0],
+                    [-5.0, 21.0, -5.0, -3.0, 63.0],
+                    [-8.0, 1.0, -16.0, 5.0, -45.0],
+                    [0.0, -2.0, 5.0, 8.0, 24.0]]
+    """
+    start_matrix = [[-19.0, 2.0, -1.0, -8.0, -38.0],
+                    [-2.0, 14.0, 0.0, -4.0, 20.0],
+                    [6.0, -5.0, -20.0, -6.0, -52.0],
+                    [-6.0, 4.0, -2.0, 15.0, 43.0]]
+
+    for i in range(len(start_matrix)):
+        for j in range(len(start_matrix)):
+            if i != j:
+                start_matrix[i][j] = -start_matrix[i][j]
+    print(start_matrix)
+    print(f"Статус удовлетворенности матрицы: {check_correct_matrix(start_matrix)}")
+    solve(matrix=start_matrix, pre_x=x_lst)
+    table.save(file_name_table)
+    table.close()
--- work_5.py
+++ work_5.py
@@ -5,3 +5,40 @@
 с 4 десятичными знаками.
 y' = (1-y^2)*cos(x) + 0.6*y__________________y(0) = 0.
 """
+import math
+
+
+# Функция правой части ODU
+def f(x, y):
+    return 1 - pow(y, 2) * math.cos(x) + 0.6 * y
+
+
+def output(answer_x: list, answer_y: list, count: int):
+    for i in range(count):
+        print(f"Value x{i} = {answer_x[i]}, Value y(x{i}) = {round(answer_y[i], 4)}")
+
+
+def runge_kutta(start_poz, end_poz, count, step_h):
+    # создание массива x_i
+    lst_x = [round(start_poz + i * h, 1) for i in range(count)]
+    print(lst_x)
+    lst_y = [start_poz]
+    for i in range(count):
+        k1 = step_h * f(lst_x[i], lst_y[i])
+        k2 = step_h * f(lst_x[i] + step_h / 2, lst_y[i] + k1 / 2)
+        k3 = step_h * f(lst_x[i] + step_h / 2, lst_y[i] + k2 / 2)
+        k4 = step_h * f(lst_x[i] + step_h, lst_y[i] + k3)
+
+        yi_1 = lst_y[i] + (k1 + 2 * k2 + 2 * k3 + k4) / 6
+        lst_y.append(yi_1)
+
+    output(answer_x=lst_x, answer_y=lst_y, count=count)
+    return 1
+
+
+if __name__ == "__main__":
+    a = 0  # y(a) = 0
+    b = 1
+    h = 0.1
+    N = int((b - a) / h)
+    runge_kutta(start_poz=a, end_poz=b, count=N+1, step_h=h)
